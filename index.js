267587217b60cb17b7d536642356d278703fd90e
陈金鑫 <369790652@qq.com>
2023-07-18 15:31:21 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { tailwindToCss, originClasses } = require("./tool");
 
-// const nativeCss = tailwindToCss(`w-[1180px] hello yourClasses px-[15px] hover:text-[red] text-[18px] text-[0.3rem] 
-// text-[#808082] mx-auto py-[10px] mx-[10px] my-[20px] 
-// border-[1px] border-[black]  flex items-center justify-between h-[64px] md:mx-auto`)
+const nativeCss = tailwindToCss(`w-[1180px] hello yourClasses px-[15px] hover:text-[red] text-[18px] text-[0.3rem] 
+text-[#808082] mx-auto py-[10px] mx-[10px] my-[20px] 
+border-[1px] border-[black]  flex items-center justify-between h-[64px] md:mx-auto`)
 
-// console.log("originClasses: ", Array.from(originClasses));
-// console.log("nativeCss: \n\n", nativeCss);
+console.log("originClasses: ", Array.from(originClasses));
+console.log("nativeCss: \n\n", nativeCss);
 
 
 const run = require("./tool/framework")
@@ -16,6 +16,6 @@ const run = require("./tool/framework")
  * @param {*} way  compile way: "inline"  "native"  "cssinjs" (only support linaria), default "inline"
  */
 
-run("/Users/pudding/前端总学习/tailwind2css/src", "vue", "inline");
+run("/Users/pudding/前端总学习/tailwind2css/src", "jsx", "inline");
 
 
