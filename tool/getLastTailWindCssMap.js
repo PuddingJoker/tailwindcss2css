8547693496750ab05c0eb824d2aeb1652ee90e37
陈金鑫 <369790652@qq.com>
2023-07-20 15:05:13 +0800

    update

--- tool/getLastTailWindCssMap.js
+++ tool/getLastTailWindCssMap.js
@@ -1,10 +1,12 @@
+//  run in https://tailwindcomponents.com/cheatsheet/  console  to get latest tailwindCssMap
+
 let obj = {}
 
 const trs = document.getElementsByTagName("tr");
 
 for (let i = 0; i < trs.length; i++) {
     const tds = trs[i].getElementsByTagName('td');
-    obj[tds[0].textContent] = tds[1].textContent.split(";").join(";\n");
+    obj[tds[0].textContent] = tds[1].textContent.split(";").filter(Boolean).join(";\n") + ';';
 }
 
 console.log(JSON.stringify(obj));
